fde2a21157a8a877fa6951ebb54f99e60a3803dc
antonvyshemirskii <71921169+antonvyshemirskii@users.noreply.github.com>
2023-01-04 22:07:15 +0200

    Add files via upload

---
+++ WebApi/DropboxFileDeleter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi
+{
+    public class DropboxFileDeleter : DropboxFactoryClass
+    {
+        public override bool CheckFileExist()
+        {
+            DropboxFileMetadata DbFMeta = new DropboxFileMetadata();
+            return DbFMeta.CheckFileExist();
+        }
+
+        public override void ShowSuccessMessage()
+        {
+            Console.WriteLine("You deleted file properly.");
+        }
+
+        public void DeleteFile()
+        {
+            if (CheckFileExist())
+            {
+                // Loop through all files in dropbox and if found one with given name return its metadata
+                var filesDb = DbClient.Files.ListFolderAsync(string.Empty);
+                foreach (var item in filesDb.Result.Entries)
+                {
+                    if (item.Name == fileName)
+                    {
+                        DbClient.Files.DeleteV2Async(item.PathLower);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WebApi/DropboxFileMetadata.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+using Dropbox.Api;
+using Dropbox.Api.FileRequests;
+using Dropbox.Api.Files;
+
+namespace WebApi
+{
+    public class DropboxFileMetadata : DropboxFactoryClass
+    {
+        public override bool CheckFileExist()
+        {
+            // Upload file and if url is not empty - it is uploaded
+            DropboxUploader DbUploader = new DropboxUploader();
+            DbUploader.UploadFile();
+
+            if (DbUploader.url.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void ShowSuccessMessage()
+        {
+            Metadata file = GetFileMetadata();
+            if (file != null)
+            {
+                Console.WriteLine("Metadata retrieved!");
+                Console.WriteLine("Name: " + file.Name);
+                Console.WriteLine("Path: " + file.PathDisplay);
+                Console.WriteLine("ID: " + file.AsFile.Id);
+                Console.WriteLine("Modified: " + file.AsFile.ClientModified);
+            } 
+        }
+
+        public Metadata GetFileMetadata()
+        {
+            if (CheckFileExist())
+            {
+                // Loop through all files in dropbox and if found one with given name return its metadata
+                var filesDb = DbClient.Files.ListFolderAsync(string.Empty);
+                foreach (var item in filesDb.Result.Entries)
+                {
+                    if (item.Name == fileName)
+                    {
+                        return item;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
